8482d3cc8309bb9bbc9ab412891663139828d4cb
mailog <m.ilog@yahoo.com>
2021-09-30 15:29:09 -0700

    Update home.js

--- home.js
+++ home.js
@@ -18,7 +18,7 @@ window.onload = function()
 	function updateBG(currScroll, background)
 	{
 		console.log("Something something scroll " + background.dataset.scrollspeed);
-		var currOffset = currScroll * background.data.scrollspeed;
+		var currOffset = currScroll * background.dataset.scrollspeed;
 		background.element.style.transform = "transform.3d(0," + currOffset + "px, 0)";
 	}
   
