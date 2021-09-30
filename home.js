6bb8fae4497dc9039420bb4007797368e7d84577
mailog <m.ilog@yahoo.com>
2021-09-30 15:39:56 -0700

    Update home.js

--- home.js
+++ home.js
@@ -19,7 +19,7 @@ window.onload = function()
 	{
 		var currOffset = currScroll * background.dataset.scrollspeed;
 		background.style.transform = "transform.3d(0," + currOffset + "px, 0)";
-		console.log("Window - " + currScroll + " - Scroll Offset " + background.dataset.scrollspeed + ": " + currOffset);
+		console.log(Date.now() + " Window - " + currScroll + " - Scroll Offset " + background.dataset.scrollspeed + ": " + currOffset);
 	}
   
 	document.addEventListener("scroll",updateScroll);
