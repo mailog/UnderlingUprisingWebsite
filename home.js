11ce162bb5ae055fcb8fbe4446bf67464d2c683a
mailog <m.ilog@yahoo.com>
2021-09-30 15:10:32 -0700

    Update home.js

--- home.js
+++ home.js
@@ -4,10 +4,9 @@ window.onload = function()
   
   //Init Background Data
   var backgrounds = document.querySelectorAll(".background");
-
-  for(var bg in backgrounds)
+	for(var i = 0; i < backgrounds.length; i++)
   {
-    console.log("Speed " + bg.dataset.scrollSpeed");
+	    console.log("Speed " + backgrounds[i].dataset.scrollspeed); 
   }
   
   function updateScroll()
