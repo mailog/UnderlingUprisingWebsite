bd1e439ac8d371c642d3c07bc6bb1956004419b0
mailog <m.ilog@yahoo.com>
2021-09-30 14:35:25 -0700

    Update home.js

--- home.js
+++ home.js
@@ -1,1 +1,22 @@
-
+window.onload = function()
+{
+  var background = [];
+  
+  //Init Background Data
+  var backgrounds = Document.querySelectorAll(".background");
+  
+  for(var bg in backgrounds)
+  {
+      background.push(bg);
+  }
+  
+  Console.Log(background.length);
+  
+  function updateScroll()
+  {
+      Console.Log("SCROLLLLLLLL!");
+  }
+  
+  document.AddEventListener("scroll",updateScroll);
+  updateScroll();
+}
